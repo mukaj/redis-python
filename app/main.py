880e260f9ab1b8e2a91f5d40df988092b956948e
mukaj <43552866+mukaj@users.noreply.github.com>
2024-04-15 14:44:04 -0400

    Migrate from synchronous to asynchronous code.

--- app/main.py
+++ app/main.py
@@ -1,21 +1,27 @@
-import socket
+import asyncio
 from .encoders import RESP_Simple_String
 
 
-def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, client_address = server_socket.accept()  # wait for client
-
+async def handle_connections(
+    reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+):
     while True:
-        # Check to see if we recieved a message from the client
-        if client_socket.recv(1024):
-            # Send PONG message to client
-            client_socket.sendall(
-                RESP_Simple_String("PONG")
-            )  # socket.sendall is used to ensure that all the bytes are sent to the client
-        else:
+        data = await reader.read(1024)
+        if not data:
             break
+
+        writer.write(RESP_Simple_String("PONG"))
+        await writer.drain()  # flush the writer
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def main():
+    server = await asyncio.start_server(handle_connections, "localhost", 6379)
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
