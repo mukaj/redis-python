266ec5e8f574a0c449f11ee4d97ac241ee3e702f
mukaj <43552866+mukaj@users.noreply.github.com>
2024-04-15 03:54:22 -0400

    Respond to multiple requests from the same client.
    The server will respond with PONG regardless of what the client sends. However it will stop after it receives nothing from the lcient.

--- app/main.py
+++ app/main.py
@@ -6,10 +6,15 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, client_address = server_socket.accept()  # wait for client
 
-    # Send PONG message to client
-    client_socket.sendall(
-        RESP_Simple_String("PONG")
-    )  # socket.sendall is used to ensure that all the bytes are sent to the client
+    while True:
+        # Check to see if we recieved a message from the client
+        if client_socket.recv(1024):
+            # Send PONG message to client
+            client_socket.sendall(
+                RESP_Simple_String("PONG")
+            )  # socket.sendall is used to ensure that all the bytes are sent to the client
+        else:
+            break
 
 
 if __name__ == "__main__":
