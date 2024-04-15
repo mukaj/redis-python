b809444b3b206f28e9d24d626e5d52d546b46f33
mukaj <43552866+mukaj@users.noreply.github.com>
2024-04-15 03:50:33 -0400

    Respond to client with "PONG" message.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 import socket
+from .encoders import RESP_Simple_String
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client_socket, client_address = server_socket.accept()  # wait for client
+
+    # Send PONG message to client
+    client_socket.sendall(
+        RESP_Simple_String("PONG")
+    )  # socket.sendall is used to ensure that all the bytes are sent to the client
 
 
 if __name__ == "__main__":
