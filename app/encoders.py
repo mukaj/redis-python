8803e4ddcd7b9130c21a521f568c84cc33375170
mukaj <43552866+mukaj@users.noreply.github.com>
2024-04-16 22:52:21 -0400

    Rename and rework encoder functions.
    * Improve code structure for encoders and add a `decode` parameter to allow function reuse.

--- app/encoders.py
+++ app/encoders.py
@@ -1,6 +1,29 @@
-def RESP_Simple_String(string: str) -> bytes:
+def simple_string(data: str, decode=False) -> bytes | str:
     """
-    Encode a string according to Redis' "RESP Simple Strings"
+    Encode and Decode a string according to Redis' "RESP Simple Strings"
     https://redis.io/docs/latest/develop/reference/protocol-spec/#simple-strings
     """
-    return bytes(f"+{string}\r\n", "utf-8")
+
+    if not decode:
+        return f"+{data}\r\n".encode(encoding="utf-8")
+    else:
+        # The simple string content is the same as the data string,
+        # only without the plus symbol in the beginning and without the
+        # CRLF characters in the end.
+        return data[1:-2]
+
+
+def bulk_string(data: str, decode=False) -> bytes | str:
+    """
+    Encode and Decode a string according to Redis' "RESP Bulk Strings"
+    https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+    """
+
+    if not decode:
+        return f"${len(data)}\r\n{data}\r\n".encode(encoding="utf-8")
+    else:
+        # The start is past the first instance of CRLF
+        start = data.find("\r\n") + 2
+        # The end is before the last instance of CRLF
+        end = data.rfind("\r\n")
+        return data[start:end]
