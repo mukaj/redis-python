32230ff1c594426cf73942700f3cbe7b95e727d7
mukaj <43552866+mukaj@users.noreply.github.com>
2024-04-19 02:59:12 -0400

    Add `Entry` class to handle expiry for entries.

--- app/database.py
+++ app/database.py
@@ -1,1 +1,9 @@
+from datetime import datetime
+
 cache = {}
+
+
+class Entry:
+    def __init__(self, data: str, timeout: datetime = None):
+        self.data: str = data
+        self.timeout: datetime = timeout
